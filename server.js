8ee2c3881aa5177a55092e51b0be3ba81fc010f1
Josh Blumberg <jlblumberg@gmail.com>
2020-06-15 15:31:02 +0100

    Add item model and schema

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const config = require('./config/config');
 
 // Connect to Mongo
 mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Mongo connected successfully"))
+  .then(() => console.log(`Mongo connected successfully to the ${process.env.NODE_ENV} database`))
   .catch(err => console.log(err));
 
 const listen = app.listen(config.port, () => console.log(`Server started on port ${config.port}`));
---
+++ tests/Item.test.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const expect = require('chai').expect;
+const Item = require('../models/Item').Item;
+
+describe('Items', () => {
+
+  it('can be saved to the test database', () => {
+    const testItem = Item({
+      name: 'test-name'
+    })
+    try {
+      testItem.save();
+    } catch (err) {
+      throw new Error(err);
+    }
+  });
+
+  it('doesn\'t save a user to the database if the field is wrong', async () => {
+    const testItem = Item({
+      blah: 'test-name'
+    })
+    try {
+      await testItem.save();
+    } catch (err) {
+      expect(err._message).to.equal('item validation failed')
+    }
+  });
+  
+});

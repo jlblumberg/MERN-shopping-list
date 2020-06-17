f5033e0cebac291670983633c0779020831e890a
Josh Blumberg <jlblumberg@gmail.com>
2020-06-17 14:28:06 +0100

    Add backend route for deleting an item

--- routes/api/items.js
+++ routes/api/items.js
@@ -23,4 +23,13 @@ router.post('/', (req, res) => {
     .then(item => res.json(item));
 });
 
+// @route DELETE api/items/:id
+// @desc delete a specific item
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id).then(item => 
+    item.remove().then(() => res.json({ success: true }))
+  )
+  .catch(err => res.status(404).json({ success: false }));
+});
+
 module.exports = router;

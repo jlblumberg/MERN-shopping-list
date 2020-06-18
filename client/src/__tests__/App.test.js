2751a95c86f2ca07953a8959d76e49040c85a7a3
Josh Blumberg <jlblumberg@gmail.com>
2020-06-18 14:45:05 +0100

    Add App (parent component)

---
+++ client/src/__tests__/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import App from '../App';
+
+describe('App', () => {
+
+  it('renders correctly', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  
+});
+
